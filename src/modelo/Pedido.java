775022f27c7a897e565e9d81871b6010327a8ccb
mostri28 <mateoetchepare@gmail.com>
2022-10-05 20:27:11 -0300

    un save q me faltó

--- src/modelo/Pedido.java
+++ src/modelo/Pedido.java
@@ -15,6 +15,14 @@ public class Pedido {
 	public void setProducto(Producto producto) {
 		this.producto = producto;
 	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
 	
 	
 	
