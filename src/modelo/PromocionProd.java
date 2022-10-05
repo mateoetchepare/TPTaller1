a4a455b19852c84ec54916bce762ffc77611ecf4
mostri28 <mateoetchepare@gmail.com>
2022-10-05 20:32:42 -0300

    en un toque sigo

---
+++ src/modelo/PromocionProd.java
@@ -0,0 +1,7 @@
+package modelo;
+
+public class PromocionProd extends Promocion {
+	private int idPromocion;
+	private Producto producto;
+	
+}
---
+++ src/modelo/PromocionTemp.java
@@ -0,0 +1,8 @@
+package modelo;
+
+public class PromocionTemp extends Promocion {
+	private String nombre;
+	private String formaPago;
+	private int porcentajeDescuento;
+	private boolean esAcumulable;
+}
